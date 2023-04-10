311ba14ba892a4dd93487c3bfab1af4ddf2d4732
alvaro-bm <=>
2023-04-10 17:31:42 -0600

    He agregado algunos puntos y comas faltantes

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,10 +1,10 @@
 (function () {
     importCSS("styles/footer.css");
     const footer = create("footer", null, null);
-    const footTitle = create("h3", null, `"Una rebanada una sonrisa"`);
-    const paragraph = create("p", null, "gracias por su preferencia");
+    const footTitle = create("h3", null, `"Una rebanada, una sonrisa"`);
+    const paragraph = create("p", null, "¡gracias por su preferencia!");
     const logo = create("img", { src: imports.use("logo") }, null);
-    const miniTitle = create("h6", null, "made with love by me :)");
+    const miniTitle = create("h6", null, "hecho con amor por Alvaro B.M.");
     insertBrothers(
         footer,
         footTitle,
